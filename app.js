917f590c80a7a18413e3544920be2f8c7c60239e
Talha Khadim <67506264+talhakhadim@users.noreply.github.com>
2023-03-02 23:13:19 +0500

    getting port from env

--- app.js
+++ app.js
@@ -47,4 +47,6 @@ io.on('connection',(socket)=>{
 })
 sequelize.sync({ force: false });
 //create server
-httpServer.listen(5000, () => console.log('Server started on port 5000'));
+
+const PORT = process.env.PORT || 5000;
+httpServer.listen(PORT, () => console.log(`Server started on port ${PORT}`));
