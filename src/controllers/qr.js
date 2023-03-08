37c66cdc8ff1ea792516687cd5390c7267f26aed
Ali Usama <101451293+Ali0548@users.noreply.github.com>
2023-03-08 20:13:37 +0500

    updated

--- src/controllers/qr.js
+++ src/controllers/qr.js
@@ -16,11 +16,6 @@ module.exports = {
         if (checkInvoice) {
             return res.json({ message: 'Invoice already exists' });
         }
-        // const checkInvoice = await youInvoiceModel.findByPk(req.body.invoiceId);
-        // if (!checkInvoice) {
-        //     return res.json({ message: 'No invoice found' });
-        // }
-        // Use the above commented code accordingly
         const uniqueId = Date.now().toString(36) + Math.random().toString(36).substr(2, 5);
         const invoice = {
             invoiceId: req.body.invoiceId,
@@ -29,10 +24,9 @@ module.exports = {
             uniqueKey: uniqueId,
             customerId: req.body.customerId,
         };
-        const invoiceData = baseUrl + 'invoice/admin/active/' + uniqueId;
+        const invoiceData = baseUrl + 'invoice/active/' + uniqueId;
         const invoiceAr = await invoiceQr.create(invoice);
         if (invoiceAr) {
-
             const folderPath = path.join(__dirname, 'temp');
             const fileName = path.join(folderPath, `${req.body.invoiceId}_${uniqueId}_invoice.png`);
             QRCode.toFile(fileName, invoiceData, function (err) {
@@ -44,98 +38,218 @@ module.exports = {
         }
 
     },
-    templateSecurity: async (req, res) => {
+    // templateSecurity: async (req, res) => {
+    //     try {
+    //         if (!req.params.id) {
+    //             return res.json({ message: 'No id found' });
+    //         }
+    //         const qrCode = req.params.id;
+    //         // const emailCode = Math.random().toString(36).substring(2, 8) + Date.now();
+    //         let emailCode = '123';
+    //         const verificationCode = await verification.update(
+    //             {
+    //                 qrcode: qrCode,
+    //                 code: emailCode,
+    //             },
+    //             {
+    //                 where: {}
+    //             }
+    //         );
+    //         if (!verificationCode) {
+
+    //             return res.json({ message: 'Something Went Wrong, Please try again' });
+    //         }
+    //         // Email needs to sent here
+    //         // Note : Sent email to abhai0548@gmail.com just sent verification code
+
+    //         res.sendFile(__dirname + '/index.html');
+    //     } catch (error) {
+    //         return res.json({ message: error.message });
+    //     }
+    // },
+    sendingInvoice: async (req, res) => {
+        // Verification
+        try {
+            let verify = await invoiceQr.findOne({
+                where: { uniqueKey: req.params.id }
+            })
+            if (!verify) {
+                return res.json({
+                    status: false,
+                    message: 'There an error with invoice check manually, Please try again'
+                })
+            }
+
+            verify = JSON.parse(JSON.stringify(verify));
+            if (verify.verification === false) {
+                return res.json({
+                    status: true,
+                    message: 'Invoice Fetched Successfully',
+                    data: {
+                        invoiceId: verify.invoiceId,
+                        userId: verify.userId,
+                        generatedAt: new Date(verify.createdAt.toString()).toLocaleString('en-US', { timeZone: 'UTC', hour12: true }),
+                    }
+                })
+            }
+
+
+            const checkifAlredy = await customerverification.findOne({
+                where: { invoiceId: verify.id }
+            })
+            if (checkifAlredy) {
+                return res.json({
+                    status: true,
+                    message: 'Invoice Fetched Successfully, Timer is already started and email has been sent already to customer',
+                    data: {
+                        invoiceId: verify.invoiceId,
+                        userId: verify.userId,
+                        generatedAt: new Date(verify.createdAt.toString()).toLocaleString('en-US', { timeZone: 'UTC', hour12: true }),
+                    }
+                })
+            }
+            const uniqueId = Date.now().toString(36) + Math.random().toString(36).substr(2, 5);
+            const invoice = {
+                invoiceId: verify.id,
+                qrcode: verify.qrCode,
+                employeeId: verify.userId,
+                customerId: verify.customerId,
+                uniqueKey: uniqueId,
+                status: 'active'
+            };
+            console.log("Invoice=====>", invoice);
+            const invoiceAr = await customerverification.create(invoice);
+            if (invoiceAr) {
+                await invoiceQr.update(
+                    {
+                        verification: false,
+                    },
+                    {
+                        where: { id: verify.id }
+                    }
+                );
+                const invoiceData = baseUrl + 'invoice/acepted/' + verify.uniqueKey;
+                const folderPath = path.join(__dirname, 'customers');
+                const fileName = path.join(folderPath, `${verify.id}_${uniqueId}_invoice.png`);
+                QRCode.toFile(fileName, invoiceData, function (err) {
+                    if (err) throw err;
+                    console.log('QR code saved as ' + fileName);
+                    // res.sendFile(fileName, { root: __dirname });
+                    // return res.json(invoiceAr);
+                });
+                return res.json({
+                    status: true,
+                    message: 'Invoice Fetched Successfully, Timer has been started and email has been sent successfully',
+                    data: {
+                        invoiceId: verify.invoiceId,
+                        userId: verify.userId,
+                        generatedAt: new Date(verify.createdAt.toString()).toLocaleString('en-US', { timeZone: 'UTC', hour12: true }),
+                    }
+                })
+            }
+        } catch (error) {
+            return res.json({ message: error.message });
+        }
+
+    },
+    customerAcept: async (req, res) => {
+        try {
+            let newverify = await invoiceQr.findOne({
+                where: { uniqueKey: req.params.id },
+            })
+
+            if (!newverify) {
+                return res.json({
+                    status: false,
+                    message: 'There an error with invoice check manually, Please try again'
+                })
+            }
+
+            let verify = JSON.parse(JSON.stringify(newverify));
+            console.log("verify====>", verify);
+            if (verify.verification === false) {
+                return res.json({
+                    status: true,
+                    message: 'Invoice Fetched Successfully',
+                    data: {
+                        invoiceId: verify.invoiceId,
+                        userId: verify.userId,
+                        generatedAt: new Date(verify.createdAt.toString()).toLocaleString('en-US', { timeZone: 'UTC', hour12: true }),
+                        // completed: new Date(verify.updatedAt.toString()).toLocaleString('en-US', { timeZone: 'UTC', hour12: true }),
+                    }
+                })
+            }
+            let already = await customerverification.findOne({
+                where: { qrcode: verify.qrCode }
+            })
+            already = JSON.parse(JSON.stringify(already));
+            if (already.status === 'active') {
+                const check = await customerverification.update({
+                    status: 'completed',
+                }, {
+                    where: { qrcode: verify.qrCode }
+                })
+                if (check) {
+                    await invoiceQr.update(
+                        {
+                            verification: false,
+                        },
+                        {
+                            where: { id: verify.id }
+                        }
+                    );
+                    return res.json({
+                        status: true,
+                        message: 'Invoice Completed Successfully',
+                    })
+                }
+            }
+            await invoiceQr.update(
+                {
+                    verification: false,
+                },
+                {
+                    where: { id: verify.id }
+                }
+            );
+            return res.json({
+                status: false,
+                message: `You cannot complete this. Invoice is already ${already.status}`,
+            })
+        } catch (error) {
+            return res.json({ message: error.message });
+        }
+
+        // Sent an email to abhai0548@gmail.com, that Invoice is acepted
+    },
+    rejected: async (req, res) => {
         try {
             if (!req.params.id) {
                 return res.json({ message: 'No id found' });
             }
             const qrCode = req.params.id;
-            // const emailCode = Math.random().toString(36).substring(2, 8) + Date.now();
-            let emailCode = '123';
-            const verificationCode = await verification.update(
+            const check = await customerverification.findOne({
+                where: { qrcode: qrCode }
+            })
+            if (check.status === 'rejected' || check.status === 'completed') {
+                return res.json({ message: `You cannot process because this invoice is already ${check.status}` });
+            }
+            const invoice = await customerverification.update(
                 {
-                    qrcode: qrCode,
-                    code: emailCode,
+                    status: 'rejected',
                 },
                 {
-                    where: {}
+                    where: { qrcode: qrCode }
                 }
             );
-            if (!verificationCode) {
-
+            if (!invoice) {
                 return res.json({ message: 'Something Went Wrong, Please try again' });
             }
-            // Email needs to sent here
-            // Note : Sent email to abhai0548@gmail.com just sent verification code
 
-            res.sendFile(__dirname + '/index.html');
+            return res.json({ message: 'Invoice Rejected Successfully' });
         } catch (error) {
             return res.json({ message: error.message });
         }
-    },
-    sendingInvoice: async (req, res) => {
-
-        // Verification
-        const verify = await verification.findOne({
-            where: { code: req.body.code, qrcode: req.body.barcode }
-        })
-        if (!verify) {
-            return res.json({
-                status: false,
-                message: 'Invalid Code'
-            })
-        }
-        await verification.update({ qrcode: null, code: null, }, {
-            where: {}
-        });
-
-        // Fetching Invoice Information
-        let invoiceDetails = await invoiceQr.findOne({
-            where: { uniqueKey: req.body.barcode }
-        })
-
-        invoiceDetails = JSON.parse(JSON.stringify(invoiceDetails));
-        console.log(invoiceDetails);
-        if (!invoiceDetails) {
-            return res.json({ status: false, message: 'Invoice not found, Please double check manually' })
-        }
-        const uniqueId = Date.now().toString(36) + Math.random().toString(36).substr(2, 5);
-        const invoice = {
-            invoiceId: invoiceDetails.id,
-            qrcode: `${invoiceDetails.id}_${invoiceDetails.userId}_${uniqueId}_invoice.png`,
-            employeeId: invoiceDetails.userId,
-            customerId: invoiceDetails.customerId,
-            uniqueKey: uniqueId,
-            status: 'active'
-        };
-        console.log("Invoice=====>", invoice);
-        const invoiceData = baseUrl + '/invoice/customer/active/' + uniqueId;
-        const invoiceAr = await customerverification.create(invoice);
-        if (invoiceAr) {
-            const folderPath = path.join(__dirname, 'customers');
-            const fileName = path.join(folderPath, `${invoiceDetails.id}_${uniqueId}_invoice.png`);
-            QRCode.toFile(fileName, invoiceData, function (err) {
-                if (err) throw err
-                // Email needs to be sent here
-                // Note: Sent email to customer, just sent a Qr Code as an image
-                return res.json({ status: true, message: 'Invoice sent to client successfully and timer is activated' })
-
-            });
-        }
-    },
-    scanByCustomer: async (req, res) => {
-        res.sendFile(__dirname + '/customer.html');
-    },
-    customerAcept: async (req, res) => {
-        const qrCode = req.params.id;
-        const customerVerification = await customerverification.findOne({
-            where: { uniqueKey: qrCode }
-        });
-        if (!customerVerification) {
-            return res.json({ message: 'Invalid Code' })
-        }
-        await customerVerification.update({ status: 'Acepted' });
-        return res.json({ message: 'Invoice Acepted' });
 
         // Sent an email to abhai0548@gmail.com, that Invoice is acepted
     },
