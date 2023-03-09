59733fda1cab6fcd87a596b78dcda200a52c139e
asad-at-989780188305 <asad.shahab@acruxsystems.co.uk>
2023-03-09 15:49:22 +0500

    update invoice api

--- src/routes/qr.js
+++ src/routes/qr.js
@@ -8,5 +8,6 @@ router.get('/rejected/:id', qr.rejected);
 router.post('/report/', qr.avgTime);
 router.get('/active/:id', qr.sendingInvoice);
 router.get('/acepted/:id', qr.customerAcept);
+router.put('/update/:id', qr.updateInvoice);
 
 module.exports = router;
